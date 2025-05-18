363944f9135bd8052a8745037f34acb18644ed3f
ebreggeman <[200~204634234+ebreggeman@users.noreply.github.com~>
2025-05-18 16:23:05 -0500

    Fix Container update Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="text-center pt-32 px-5">
-      <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to my blog </h1>
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to the Venting Portal </h1>
       <p className="max-w-[750px] mx-auto leading-8">A place to vent</p>
     </main>
   );
