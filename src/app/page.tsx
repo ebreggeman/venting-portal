58f8f08cedd5d8f5751cc240c10e3a10609026f7
ebreggeman <[200~204634234+ebreggeman@users.noreply.github.com~>
2025-05-25 13:09:09 -0500

    Add upvote button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to the Venting Portal </h1>
       <p className="max-w-[750px] mx-auto leading-8">A place to vent</p>
+
     </main>
   );
 }
