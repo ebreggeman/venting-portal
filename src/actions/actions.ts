ad3f82c48e14b8718b7acc47a167cc74516c3b08
ebreggeman <[200~204634234+ebreggeman@users.noreply.github.com~>
2025-07-04 16:45:06 -0500

    Fix errors

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,8 +1,14 @@
 "use server";
-
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 import prisma from "../lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function createPost(formData: FormData) {
+    const { isAuthenticated } = getKindeServerSession()
+    if (!(await isAuthenticated())) {
+        redirect('api/auth/login');
+    }
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
     console.log(title, body);
@@ -14,6 +20,9 @@ export async function createPost(formData: FormData) {
             body,
         },
     });
+    //revalidate
+    revalidatePath("/posts");
+
 }
 
 
