c64e365cd2174bd63a24873e37d854b73c873044
ebreggeman <[200~204634234+ebreggeman@users.noreply.github.com~>
2025-07-12 12:26:57 -0500

    Restart server

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,10 @@
 export const dynamic = "force-dynamic";
 import PostsList from "../../components/posts-list";
-import { Suspense } from "react";
 export default async function Page() {
 
 
     return <main className="text-center pt-16 px-5">
         <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
-        {/* Remove Suspense since PostsList is async and must be awaited */}
         {await PostsList()}
     </main>
 }
